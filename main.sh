f4d7a540397ba269cd30ac62983f98f3e042f4a8
Knut-Frode Dagestad <knutfd@met.no>
2026-05-19 17:54:04 +0200

    Added stow to dependencies

--- main.sh
+++ main.sh
@@ -5,7 +5,7 @@ export MAMBA_ROOT_PREFIX=$HOME/.mconda3
 
 sudo apt-get update
 
-sudo apt-get install -y git build-essential curl wget tmux git
+sudo apt-get install -y git build-essential curl wget tmux git stow
 
 # mamba / python
 echo "python."
@@ -22,7 +22,7 @@ echo "install nvm, node"GENT.md
 echo "--- nvm"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 # sourcing is the usual way to do it, but does not work within the script (probably because of bashrc interactive session check)
-# source ~/.bashrc
+source ~/.bashrc
 # load nvm explicitly!
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
@@ -44,17 +44,6 @@ copilot --allow-all-tools --version
 # mkdir dev/
 # cd dev/
 # git clone https://github.com/knutfrode/met-dev-setup.git
-
-# set up gh
-(type -p wget >/dev/null || (sudo apt update && sudo apt install wget -y)) \
-	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
-	&& out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
-	&& cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
-	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
-	&& sudo mkdir -p -m 755 /etc/apt/sources.list.d \
-	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-	&& sudo apt update \
-	&& sudo apt install gh -y
 
 STOWT="stow -t /home/$USER"
 cd ~/met-dev-setup/
